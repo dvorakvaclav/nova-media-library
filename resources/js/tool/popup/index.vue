14876bd839664b4952415d05db0526d82128fec3
Vaclav Dvorak <vasek@edgy.digital>
2023-07-10 13:05:24 +0200

    Fixed popup css

--- resources/js/tool/popup/index.vue
+++ resources/js/tool/popup/index.vue
@@ -1,41 +1,41 @@
 <template>
-  <div class="popup fixed top-10 left-0 right-0 pin z-20 py-view bg-primary-70% overflow-y-auto">
+  <div class="popup fixed top-10 left-0 right-0 pin z-20 py-view bg-primary-70% overflow-y-auto" style="top: 2.5rem;">
     <div class="absolute pin z-20" @click="$parent.$parent.item = null"></div>
 
-    <div class="relative z-30 bg-white p-8 rounded-lg shadow-lg m-auto">
+    <div class="relative z-30 bg-white p-8 rounded-lg shadow-lg m-auto" style="margin: auto;">
 
       <button type="button" class="nml-close select-none" @click="$parent.$parent.item = null">&times;</button>
 
-      <div class="flex border-b border-40">
-        <div class="w-1/4 py-4 font-bold text-80">{{ __('ID') }}</div>
-        <div class="w-3/4 py-4">{{ $parent.$parent.item.id }}</div>
+      <div class="md:flex md:flex-row border-b border-40">
+        <div class="w-full md:w-1/4 py-4 font-bold text-80">{{ __('ID') }}</div>
+        <div class="w-full md:w-3/4 py-4">{{ $parent.$parent.item.id }}</div>
       </div>
 
-      <div class="flex border-b border-40">
-        <div class="w-1/4 py-4 font-bold text-80">{{ __('Uploaded') }}</div>
-        <div class="w-3/4 py-4">{{ $parent.$parent.item.created }}</div>
+      <div class="md:flex md:flex-row border-b border-40">
+        <div class="w-full md:w-1/4 py-4 font-bold text-80">{{ __('Uploaded') }}</div>
+        <div class="w-full md:w-3/4 py-4">{{ $parent.$parent.item.created }}</div>
       </div>
 
-      <div class="flex border-b border-40">
-        <div class="w-1/4 py-4 font-bold text-80">{{ __('Type') }}</div>
-        <div class="w-3/4 py-4">{{ $parent.$parent.item.type }}</div>
+      <div class="md:flex md:flex-row border-b border-40">
+        <div class="w-full md:w-1/4 py-4 font-bold text-80">{{ __('Type') }}</div>
+        <div class="w-full md:w-3/4 py-4">{{ $parent.$parent.item.type }}</div>
       </div>
 
-      <div class="flex border-b border-40">
-        <div class="w-1/4 py-4 font-bold text-80">{{ __('Size') }}</div>
-        <div class="w-3/4 py-4">{{ $parent.$parent.item.options.size }}</div>
+      <div class="md:flex md:flex-row border-b border-40">
+        <div class="w-full md:w-1/4 py-4 font-bold text-80">{{ __('Size') }}</div>
+        <div class="w-full md:w-3/4 py-4">{{ $parent.$parent.item.options.size }}</div>
       </div>
 
-      <div class="flex border-b border-40">
-        <div class="w-1/4 py-4 font-bold text-80 leading-36">{{ __('Title') }}</div>
-        <div class="w-3/4 py-4">
+      <div class="md:flex md:flex-row border-b border-40">
+        <div class="w-full md:w-1/4 py-4 font-bold text-80 leading-36">{{ __('Title') }}</div>
+        <div class="w-full md:w-3/4 py-4">
           <input class="form-control form-input w-full shadow-md" v-model="$parent.$parent.item.title">
         </div>
       </div>
 
-      <div class="flex border-b border-40" v-if="$parent.$parent.config.can_private">
-        <div class="w-1/4 py-4 font-bold text-80">{{ __('Private') }}</div>
-        <div class="w-3/4 py-4">
+      <div class="md:flex md:flex-row border-b border-40" v-if="$parent.$parent.config.can_private">
+        <div class="w-full md:w-1/4 py-4 font-bold text-80">{{ __('Private') }}</div>
+        <div class="w-full md:w-3/4 py-4">
           <checkbox
             class="cursor-pointer"
             :checked="$parent.$parent.item.private"
@@ -44,9 +44,9 @@
         </div>
       </div>
 
-      <div class="flex border-b border-40" v-if="`folders` === $parent.$parent.config.store">
-        <div class="w-1/4 py-4 font-bold text-80 leading-36">{{ __('Move to folder') }}</div>
-        <div class="w-3/4 py-4">
+      <div class="md:flex md:flex-row border-b border-40" v-if="`folders` === $parent.$parent.config.store">
+        <div class="w-full md:w-1/4 py-4 font-bold text-80 leading-36">{{ __('Move to folder') }}</div>
+        <div class="w-full md:w-3/4 py-4">
           <select v-model="folder" class="shadow-md block border-0 cursor-pointer form-control form-select w-full">
             <option :value="null"></option>
             <option
@@ -59,9 +59,9 @@
         </div>
       </div>
 
-      <div class="flex border-b border-40">
-        <div class="w-1/4 py-4 font-bold text-80">{{ __('Url') }}</div>
-        <div class="w-3/4 py-4">
+      <div class="md:flex md:flex-row border-b border-40">
+        <div class="w-full md:w-1/4 py-4 font-bold text-80">{{ __('Url') }}</div>
+        <div class="w-full md:w-3/4 py-4">
           <a class="text-primary font-bold no-underline" :href="$parent.$parent.item.url" target="_blank">{{ __('Open') }}</a>
           <button type="button" class="text-80 font-bold no-underline float-right"
                   v-clipboard:copy="$parent.$parent.item.url" v-clipboard:success="onCopy">{{ __('Copy') }}</button>
@@ -69,7 +69,7 @@
       </div>
 
       <div class="flex mt-6">
-        <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded btn-primary cursor-pointer mr-6 shadow-md"
+        <button type="button" class="shadow relative bg-primary-500 hover:bg-primary-400 text-white dark:text-gray-900 cursor-pointer rounded text-sm font-bold focus:outline-none focus:ring ring-primary-200 dark:ring-gray-600 inline-flex items-center justify-center h-9 px-3 shadow relative bg-primary-500 hover:bg-primary-400 text-white dark:text-gray-900 mr-6"
                 @click="update">
           {{ __('Update') }}
         </button>
